46b45b6b1ff29ccdbeebaf27d1f4cc4e48ca346c
Adithyan Satheesh <talesofadhi@gmail.com>
2024-09-03 06:32:22 +0530

    time to deploy

--- Wapp.js
+++ Wapp.js
@@ -1,13 +1,35 @@
 const apiKey = "a9fd3d2d1e1f74381bda6ea932dabf48";  //It is the weather api code 
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=germany&appid=a9fd3d2d1e1f74381bda6ea932dabf48&"; // It is the weather appid.
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="; // It is the weather appid.
+
+       
+const searchbox = document.querySelector(".search_bar input");
+const searchbtn = document.querySelector(".search_bar button");
+
+
 
 //Then we use async function and add fuction to it.
-async function checkweather(){  
-    const response = await fetch(apiUrl + `&{apiKey}`);
+async function checkweather(city){  
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     // And add variable and fetch the api url and api key
-    var data = await response.json();
+    var data = await response.json(); // converts the weather details to json format.
     console.log(data);
-
-
+    // now we are going to fetch the weather details and give it to the weather app.
+    document.querySelector(".city").innerHTML=data.name;
+    document.querySelector(".temp").innerHTML=Math.round(data.main.temp)+ "°C";
+    document.querySelector(".humidity").innerHTML=data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML=data.wind.speed + "Km/h";
+ 
 }
-checkweather();
+searchbtn.addEventListener("click", ()=> {
+    checkweather(searchbox.value);
+    
+
+})
+
+
+
+
+
+
+
+
